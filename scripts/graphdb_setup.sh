6abc9157b3b89943a0696e9baf01d9e609e7c0b8
Alyssa Dai <alyssa.ydai@gmail.com>
2024-02-15 17:15:24 -0500

    echo statements for logging script progress

--- scripts/graphdb_setup.sh
+++ scripts/graphdb_setup.sh
@@ -124,6 +124,7 @@ RUN_USER_SETUP=$_arg_run_user_setup
 
 # Set the environment variables in the shell, to use in the script
 source "${ENV_FILE_PATH}"
+echo "Environment variables have been set from ${ENV_FILE_PATH}."
 
 # Extract just the database name
 DB_NAME="${NB_GRAPH_DB#repositories/}"
@@ -132,15 +133,20 @@ NB_GRAPH_PORT_HOST=${NB_GRAPH_PORT_HOST:-7200}
 # Get the directory of this script to be able to find the data-config_template.ttl file
 SCRIPT_DIR=$(dirname "$0")
 
+echo "The GraphDB server is being accessed at http://localhost:${NB_GRAPH_PORT_HOST}."
 
 ##### First time GraphDB setup #####
 
 if [ "${RUN_USER_SETUP}" = "on" ]; then
     # 1. Change database admin password and allow only authenticated users access
+    echo "First time GraphDB user setup enabled."
+    echo "Changing the admin password..."
     curl -X PATCH --header 'Content-Type: application/json' http://localhost:${NB_GRAPH_PORT_HOST}/rest/security/users/admin -d "{\"password\": \""${NEW_ADMIN_PASS}"\"}"
+    echo "Allowing only authenticated users access..."
     curl -X POST --header 'Content-Type: application/json' -d true http://localhost:${NB_GRAPH_PORT_HOST}/rest/security
 
     # 2. Create a new database user
+    echo "Creating a new database user ${NB_GRAPH_USERNAME}..."
     curl -X POST --header 'Content-Type: application/json' -u "admin:${NEW_ADMIN_PASS}" -d @- http://localhost:${NB_GRAPH_PORT_HOST}/rest/security/users/${NB_GRAPH_USERNAME} <<EOF
     {
         "username": "${NB_GRAPH_USERNAME}",
@@ -154,12 +160,17 @@ fi
 
 # 3. Create and save custom configuration file for the new database
 # TODO: Should we add a suffix to data-config.ttl for the db name?
+echo "Creating a GraphDB configuration file (./data-config.ttl) for the new database ${DB_NAME}: ..."
 sed 's/rep:repositoryID "my_db" ;/rep:repositoryID "'"${DB_NAME}"'" ;/' ${SCRIPT_DIR}/data-config_template.ttl > data-config.ttl
 
 # 4. Create a new database and give newly created user access permission
 # Assumes data-config.ttl is in the same directory as this script!
+echo "Creating the GraphDB database ${DB_NAME}..."
 curl -X PUT -u "admin:${NEW_ADMIN_PASS}" http://localhost:${NB_GRAPH_PORT_HOST}/${NB_GRAPH_DB} --data-binary "@data-config.ttl" -H "Content-Type: application/x-turtle"
+echo "Granting user ${NB_GRAPH_USERNAME} read/write permissions to database ${DB_NAME}..."
 curl -X PUT --header 'Content-Type: application/json' -d "
 {\"grantedAuthorities\": [\"WRITE_REPO_${DB_NAME}\",\"READ_REPO_${DB_NAME}\"]}" http://localhost:${NB_GRAPH_PORT_HOST}/rest/security/users/${NB_GRAPH_USERNAME} -u "admin:${NEW_ADMIN_PASS}"
+
+echo "Done."
 
 # ] <-- needed because of Argbash
