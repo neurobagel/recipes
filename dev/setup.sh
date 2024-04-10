5cbc922670d1aba4e2e3fe84aa054eeb9cf89d47
Alyssa Dai <alyssa.ydai@gmail.com>
2024-04-10 14:58:55 -0400

    write output of setup.sh to log

--- dev/setup.sh
+++ dev/setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "Setting up a Neurobagel graph backend..."
-
 /opt/graphdb/dist/bin/graphdb -Dgraphdb.home=/opt/graphdb/ &
 GRAPHDB_PID=$!
 
@@ -10,19 +8,26 @@ while ! curl --silent "localhost:${NB_GRAPH_PORT_HOST}/rest/repositories" | grep
     :
 done
 
-echo "Running GraphDB server setup..."
-./graphdb_setup.sh "${NB_GRAPH_ADMIN_PASSWORD}"
-echo "Finished server setup."
+# Logic for main setup
+main() {
+    echo -e "Setting up a Neurobagel graph backend...\n"
 
-echo "Adding datasets to the database..."
-./add_data_to_graph.sh ./data localhost:${NB_GRAPH_PORT_HOST} ${NB_GRAPH_DB} "${NB_GRAPH_USERNAME}" "${NB_GRAPH_PASSWORD}"
-echo "Finished adding datasets to databases."
+    echo "Setting up GraphDB server..."
+    ./graphdb_setup.sh "${NB_GRAPH_ADMIN_PASSWORD}"
+    echo "Finished server setup."
 
-echo "Adding Neurobagel vocabulary to the database"
-./add_data_to_graph.sh ./vocab localhost:${NB_GRAPH_PORT_HOST} ${NB_GRAPH_DB} "${NB_GRAPH_USERNAME}" "${NB_GRAPH_PASSWORD}"
-echo "Finished adding the Neurobagel vocabulary to the database."
+    echo "Adding datasets to the database..."
+    ./add_data_to_graph.sh ./data localhost:${NB_GRAPH_PORT_HOST} ${NB_GRAPH_DB} "${NB_GRAPH_USERNAME}" "${NB_GRAPH_PASSWORD}"
+    echo "Finished adding datasets to databases."
 
-echo "Finished setting up the Neurobagel graph backend."
+    echo "Adding Neurobagel vocabulary to the database"
+    ./add_data_to_graph.sh ./vocab localhost:${NB_GRAPH_PORT_HOST} ${NB_GRAPH_DB} "${NB_GRAPH_USERNAME}" "${NB_GRAPH_PASSWORD}"
+    echo "Finished adding the Neurobagel vocabulary to the database."
+
+    echo "Finished setting up the Neurobagel graph backend."
+}
+
+main 2>&1 | tee -a ./NEUROBAGEL_SETUP_LOG.txt
 
 # We don't have jobcontrol here, so can't bring GraphDB back to foreground
 # instead we'll wait
