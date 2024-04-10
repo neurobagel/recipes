b71cc6d8d275c4389ba7d6d4bc036215704bfb9f
Alyssa Dai <alyssa.ydai@gmail.com>
2024-04-10 11:40:13 -0400

    replace hardcoded params inside setup.sh with env vars

--- dev/setup.sh
+++ dev/setup.sh
@@ -6,20 +6,20 @@ echo "Setting up a Neurobagel graph backend..."
 GRAPHDB_PID=$!
 
 # Waiting for GraphDB to start
-while ! curl --silent "localhost:7200/rest/repositories" | grep '\[\]'; do
+while ! curl --silent "localhost:${NB_GRAPH_PORT_HOST}/rest/repositories" | grep '\[\]'; do
     :
 done
 
 echo "Running GraphDB server setup..."
-./graphdb_setup.sh apple
+./graphdb_setup.sh "${NB_GRAPH_ADMIN_PASSWORD}"
 echo "Finished server setup."
 
 echo "Adding datasets to the database..."
-./add_data_to_graph.sh ./data localhost:7200 repositories/my_db DBUSER DBPASSWORD
+./add_data_to_graph.sh ./data localhost:${NB_GRAPH_PORT_HOST} ${NB_GRAPH_DB} "${NB_GRAPH_USERNAME}" "${NB_GRAPH_PASSWORD}"
 echo "Finished adding datasets to databases."
 
 echo "Adding Neurobagel vocabulary to the database"
-./add_data_to_graph.sh ./vocab localhost:7200 repositories/my_db DBUSER DBPASSWORD
+./add_data_to_graph.sh ./vocab localhost:${NB_GRAPH_PORT_HOST} ${NB_GRAPH_DB} "${NB_GRAPH_USERNAME}" "${NB_GRAPH_PASSWORD}"
 echo "Finished adding the Neurobagel vocabulary to the database."
 
 echo "Finished setting up the Neurobagel graph backend."
